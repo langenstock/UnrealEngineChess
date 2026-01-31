872f3246c949424a4848570f0cd4be00c983f259
DL_Ultimate <langenstock@gmail.com>
2026-01-31 12:20:42 +1100

    removed unused classes

--- Source/Chess/Bishop.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Bishop.h"
-
-ABishop::ABishop()
-{
-	ePiece = EPiece::Bishop;
-}
--- Source/Chess/PawnPiece.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PawnPiece.h"
-
-APawnPiece::APawnPiece()
-{
-	ePiece = EPiece::Pawn;
-}
